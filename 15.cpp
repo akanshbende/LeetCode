e387d06dd85ece22a072b42c070c1b9829548b5c
Akansh <akanshbende29@gmail.com>
2023-08-27 23:03:19 +0530

    27-08-2023

---
+++ 15.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/3sum/description/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    unordered_map<int, int> mp;
+    vector<vector<int>> ans;
+    set<vector<int>> st;
+    vector<int> v;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        for (int j = i + 1; j < nums.size(); ++j)
+        {
+            if (i != j && mp.find(-nums[i] - nums[j]) != mp.end() && mp[-nums[i] - nums[j]] != i && mp[-nums[i] - nums[j]] != j)
+            {
+                v.push_back(nums[i]);
+                v.push_back(nums[j]);
+                v.push_back(-nums[i] - nums[j]);
+                sort(v.begin(), v.end());
+                st.insert(v);
+                v.clear();
+            }
+            mp[nums[j]] = j;
+        }
+    }
+    for (auto i : st)
+    {
+        ans.push_back(i);
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> v = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ans = threeSum(v);
+
+    return 0;
+}
