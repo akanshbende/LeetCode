52e293e9fdb9d953deed87b76d3f92169d4bf930
Akansh <akanshbende29@gmail.com>
2023-10-07 11:29:56 +0530

    7-10-23 2nd

---
+++ 78.cpp
@@ -0,0 +1,41 @@
+// 78. Subsets
+// https: // leetcode.com/problems/subsets/description/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void Solve(int idx, vector<int> &res, vector<vector<int>> &ans, vector<int> &nums, int n)
+{
+    if (idx == n)
+    {
+        ans.push_back(res);
+        return;
+    }
+    res.push_back(nums[idx]);
+    Solve(idx + 1, res, ans, nums, n);
+    res.pop_back();
+    Solve(idx + 1, res, ans, nums, n);
+}
+vector<vector<int>> subsets(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+    vector<int> res;
+    Solve(0, res, ans, nums, n);
+    return ans;
+}
+int main()
+{
+    vector<int> v = {1, 2, 3};
+    vector<vector<int>> answer = subsets(v);
+    for (auto vec : answer)
+    {
+        // cout << "[ ";
+        for (auto num : vec)
+        {
+            cout << num;
+        }
+        cout << endl;
+        // cout << " ]";
+    }
+    return 0;
+}
