debceba0b83b0d3a630df5144eb59c1a9880b7ff
Akansh <akanshbende29@gmail.com>
2023-10-12 08:55:04 +0530

    12-10-23 1st

--- 88.cpp
+++ 88.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 {
-     for(int i=0;i<n;i++){
-          // start with 0 i.e from n
-                nums1[m+i]=nums2[i];
-            
-        }
-        sort(nums1.begin(),nums1.end());
+    for (int i = 0; i < n; i++)
+    {
+        // start with 0 i.e from n
+        nums1[m + i] = nums2[i];
+    }
+    sort(nums1.begin(), nums1.end());
 }
 int main()
 {
@@ -18,7 +18,7 @@ int main()
     vector<int> nums2 = {2, 5, 6};
     int n = 3;
 
-    vector ans = merge(nums1, m, nums2, n);
+    merge(nums1, m, nums2, n);
 
     return 0;
 }
